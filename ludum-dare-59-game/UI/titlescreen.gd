44d79145cc5f914ed0d51e884a643bb7a74173d2
the-ethan-wang <ethanwang314159@gmail.com>
2026-04-20 16:02:19 +1000

    instructions

--- ludum-dare-59-game/UI/titlescreen.gd
+++ ludum-dare-59-game/UI/titlescreen.gd
@@ -1,13 +1,16 @@
 extends Control
 
-func _on_quit_pressed() -> void :
-	get_tree().quit()
-
 func _on_full_toggled(_toggled_on: bool) -> void :
 	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 
+func _on_quit_pressed() -> void :
+	get_tree().quit()
+	
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/levelselection.tscn")
+	get_tree().change_scene_to_file("res://UI/instructions.tscn")
+
+func _on_settings_pressed() -> void:
+	get_tree().change_scene_to_file("res://UI/settings.tscn")
